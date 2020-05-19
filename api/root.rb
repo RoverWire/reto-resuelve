53f6668a74ba63f3709377a8400318d0361bd7dc
Luis Felipe Perez <slashcat@gmail.com>
2020-05-19 13:21:38 -0500

    handler for EOF Error on empty requests

--- api/root.rb
+++ api/root.rb
@@ -1,12 +1,16 @@
 require 'grape'
 require 'grape-swagger'
+require 'pry'
 require_relative './v1/payrolls'
 
 module API
   class Root < Grape::API
-    format :json
+    default_format :json
     prefix :api
-    rescue_from :all
+    rescue_from EOFError do |e|
+      # Prevent empty request handling
+      error!("#{e} due empty request", 400)
+    end
 
     mount API::V1::Payrolls
   end
